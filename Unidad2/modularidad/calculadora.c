5afe1edec06520a6ebf2cebf56e9e19e3ae1ad22
Diego Alberto Lopez <dalbertolopez@unipacifico.edu.co>
2026-03-14 17:28:49 -0500

    Taller modularidad

---
+++ Unidad2/modularidad/calculadora.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <math.h>
+#include "areas.h"
+#include "volumenes.h"
+
+float areaCirculo(float radio){
+    return 3.1416 * radio * radio;
+}
+
+float areaCuadrado(float lado){
+    return lado * lado;
+}
+
+float volumenEsfera(float radio){
+    return (4.0/3.0) * 3.1416 * pow(radio,3);
+}
+
+float volumenCubo(float lado){
+    return lado * lado * lado;
+}
+
+int main(){
+
+    float radio = 5;
+    float lado = 4;
+
+    printf("Area del circulo: %.2f\n", areaCirculo(radio));
+    printf("Area del cuadrado: %.2f\n", areaCuadrado(lado));
+
+    printf("Volumen de la esfera: %.2f\n", volumenEsfera(radio));
+    printf("Volumen del cubo: %.2f\n", volumenCubo(lado));
+
+    return 0;
+}
